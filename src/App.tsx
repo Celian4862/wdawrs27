805a22be78670072e823f0ce79bbd52cec5c65ec
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2026-08-31 14:46:30 +0800

    Deleted several files, updated App.tsx
    - Changed "tile_number" to "id" in TileInfo, added a Boolean "revealed" field.
    - Changed constant "hurricane" from "hurricane" to a tornado / storm emoji
    - Replaced the Board component with the actual TSX.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import { Board } from "./components/Board";
-import { hurricane } from "./magic-strings";
-import type { TileInfo } from "./types/TileInfo";
+type TileInfo = {
+  id: number;
+  revealed: boolean;
+  tile_type: string;
+};
+
+const hurricane = "🌪";
 
 export default function App() {
   const board: TileInfo[] = Array.from({ length: 25 }, (_, i) => ({
-    tile_number: i,
+    id: i,
+    revealed: false,
     tile_type: "",
   }));
   board[12].tile_type = hurricane;
-  return <Board board={board} />;
+  return (
+    <div className="grid grid-cols-5 w-fit">
+      {board.map((tile, i) => (
+        <button
+          key={i}
+          type="button"
+          disabled={tile.id === 12}
+          className="border size-20"
+          onClick={() => console.log("Yuh")}
+        >
+          {tile.tile_type}
+        </button>
+      ))}
+    </div>
+  );
 }
--- src/types/TileInfo.ts
+++
@@ -1,4 +0,0 @@
-export type TileInfo = {
-  tile_number: number;
-  tile_type: string;
-};
