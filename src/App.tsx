1289847ff9d5fab01faba39ba83c3f2de6e97309
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2026-08-31 12:01:37 +0800

    Simplified the mapping function in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { hurricane } from "./magic-strings";
 import type { TileInfo } from "./types/TileInfo";
 
 export default function App() {
-  const board: TileInfo[] = Array.from({ length: 25 }, (_, i) => {
-    return { tile_number: i, tile_type: "" };
-  });
+  const board: TileInfo[] = Array.from({ length: 25 }, (_, i) => ({
+    tile_number: i,
+    tile_type: "",
+  }));
   board[12].tile_type = hurricane;
   return <Board board={board} />;
 }
