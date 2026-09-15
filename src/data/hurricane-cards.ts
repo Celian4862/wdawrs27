5c5f1ac845b33b3117f16a6c29ccd2069c808cf2
Celian4862 <34357791+Celian4862@users.noreply.github.com>
2026-09-15 23:06:06 +0800

    Added hurricane cards

---
+++ src/data/hurricane-cards.ts
@@ -0,0 +1,38 @@
+type HurricaneCard = {
+  id: number;
+  name: string;
+};
+
+export const hurricaneCards: HurricaneCard[] = [
+  { id: 0, name: "Thirst" },
+  { id: 1, name: "Thirst" },
+  { id: 2, name: "Thirst" },
+  { id: 3, name: "Thirst" },
+  { id: 4, name: "Hurricane Up" },
+  { id: 5, name: "Hurricane Up" },
+  { id: 6, name: "Hurricane Up" },
+  { id: 7, name: "Move 1 Up" },
+  { id: 8, name: "Move 1 Up" },
+  { id: 9, name: "Move 1 Up" },
+  { id: 10, name: "Move 1 Down" },
+  { id: 11, name: "Move 1 Down" },
+  { id: 12, name: "Move 1 Down" },
+  { id: 13, name: "Move 1 Left" },
+  { id: 14, name: "Move 1 Left" },
+  { id: 15, name: "Move 1 Left" },
+  { id: 16, name: "Move 1 Right" },
+  { id: 17, name: "Move 1 Right" },
+  { id: 18, name: "Move 1 Right" },
+  { id: 19, name: "Move 2 Up" },
+  { id: 20, name: "Move 2 Up" },
+  { id: 21, name: "Move 2 Down" },
+  { id: 22, name: "Move 2 Down" },
+  { id: 23, name: "Move 2 Left" },
+  { id: 24, name: "Move 2 Left" },
+  { id: 25, name: "Move 2 Right" },
+  { id: 26, name: "Move 2 Right" },
+  { id: 27, name: "Move 3 Up" },
+  { id: 28, name: "Move 3 Down" },
+  { id: 29, name: "Move 3 Left" },
+  { id: 30, name: "Move 3 Right" },
+];
