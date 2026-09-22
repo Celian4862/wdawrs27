b87f9350a3172e20b2a7876d73d3564ef0dbbffd
Celian4862 <134357791+Celian4862@users.noreply.github.com>
2026-09-23 01:42:27 +0800

    Added back the "listen" call for internal testing

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,12 @@
 import { Elysia } from "elysia";
 
-export default new Elysia().get("/", () => "Hello Elysia");
+const app = new Elysia().get("/", () => "Hello Elysia");
+
+if (import.meta.main) {
+  app.listen(3000);
+  console.log(
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+  );
+}
+
+export default app;
